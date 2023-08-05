30db64a24f75763f0a754c2a569536973b6f6501
shamsiddinhakimov <shamsiddinhakimov@gmail.com>
2023-08-05 15:27:32 +0500

    htfhthththghtfhtfhtfh

---
+++ js/app.js
@@ -0,0 +1,269 @@
+// let son = +prompt("Son kiritin")
+
+// if(son>0){
+//   alert(`${son+1}`)
+// }
+// else{
+//   alert(`${son-2}`)
+// }
+// let son1 = +prompt("Birinchi soni kirin")
+
+// let son2 = +prompt("Ikkinchi soni kirin")
+
+// let son3 = +prompt("Uchinchi soni kirin")
+
+// if(son1>0){
+//   console.log("Birinchisi son musbat");
+// }
+// else if (son1<0){
+//   console.log("Birinchisi son manfiy");
+// }
+// if(son2>0){
+//   console.log("Ikkinchi son musbat");
+// }
+// else if (son2<0){
+//   console.log("Ikkinchi son manfiy");
+// }
+// if(son3>0){
+//   console.log("Uchinchi son musbat");
+// }
+// else if (son3<0){
+//   console.log("Uchinchi son manfiy");
+// }
+
+// let son1 = +prompt("Birinchi soni kirin")
+
+// let son2 = +prompt("Ikkinchi soni kirin")
+
+// let son3 = +prompt("Uchinchi soni kirin")
+
+// if(son1>son2){
+//   if(son1>son3){
+//    if(son2>son3){
+//     console.log(`son1>son2>son3`)
+//    }
+//    else{
+//     console.log(`son1>son3>son2`)
+//    }
+//   }
+// }
+// else if(son2>son1){
+//   if(son2>son3){
+//    if(son3>son1){
+//      console.log(`son2>son3>son1`);
+//    }
+//    else{ console.log(`son2>son1>son3`)}
+//   }
+// }
+// else if(son3>son1){
+//   if(son3>son2){
+//    if(son1>son2){
+//     console.log(`son3>son1>son2`);
+//    }
+//    else{ console.log(`son3>son2>son1`)}
+//   }
+// }
+//if 4 ni tushunmdim
+//if 5 ni tushunmdim
+
+//while 1 chisi
+// let son1 = +prompt("Brinchi soni bering")
+
+// let son2 = +prompt("Ikkinchi soni bering")
+
+// let i = 0
+// while(i<=son1){
+//     i++
+
+    
+// }
+// if(son1>=0){
+//     if(son1>son2){
+//        console.log(`${Math.floor(son1 / son2)}`) 
+//      }
+//      else{
+//        console.log(`${Math.floor(son2 / son1)}`)  
+//      }
+// }
+// console.log(`qoldiq:${son1%son2}`)
+//while-2
+
+
+
+//while-3
+// let son1 = +prompt("Brinchi soni bering")
+
+// let son2 = +prompt("Ikkinchi soni bering")
+
+// let i = 0
+// while(i<=son1){
+//     i++
+
+    
+// }
+// if(son1>=0){
+//     if(son1>son2){
+//        console.log(`${Math.floor(son1 / son2)}`) 
+//      }
+//      else{
+//        console.log(`${Math.floor(son2 / son1)}`)  
+//      }
+// }
+// console.log(`qoldiq:${son1%son2}`)
+// while-4
+
+
+
+
+//while-5
+// let son = +prompt("Soni  kiritin")
+
+
+// let s=0
+
+// let i=3
+
+// while(i<son){
+//   i+3
+   
+//   console.log(`${son-i}`);
+
+   
+// }
+
+
+// if(son=0){
+//     console.log("Bu son 3 ni daragasi");
+// }
+// else if(son=3){
+//     console.log("Bu son 3 ni daragasi");
+// }
+
+// else{
+//     console.log("Bu son 3 ni daragasi emas");
+// }
+
+//while-6
+// let son1 = +prompt("Brinchi soni bering")
+
+// let son2 = +prompt("Ikkinchi soni bering")
+
+// let i = 0
+// while(i<=son1){
+//     i++
+
+    
+// }
+// if(son1>=0){
+//     if(son1>son2){
+//        console.log(`${Math.floor(son1 / son2)}`) 
+//      }
+//      else{
+//        console.log(`${Math.floor(son2 / son1)}`)  
+//      }
+// }
+// console.log(`qoldiq:${son1%son2}`)
+
+// while-7
+
+// let son = +prompt("Son kiritin")
+
+
+
+// i=son
+
+// while(i<=son){
+//    i++
+//  console.log(`${((son%10)*10)+((son/10)-(son%10)/10)}`) 
+
+
+// }
+
+
+//while-8
+
+// let son = +prompt("Son kiritin")
+
+// let a =((son/10)-(son%10)/10)
+
+// let s =((son%10)*1)
+
+
+// i=son
+
+// while(i<=son){
+//    i++
+//  console.log(`${a}`) 
+
+//  console.log(`${s}`)
+
+//  console.log(`${a+s}`);
+// }
+
+//while-9
+// let son = +prompt("Son kiritin")
+
+
+// let a =((son/10)-(son%10)/10)
+
+// let s =((son%10)*1)
+
+// i=son
+
+// while(i<=son){
+//    i++
+//    console.log(a);
+
+//    console.log(s)
+  
+// }
+
+
+
+// if(a==2){
+//     console.log("HA IKKI BOR");
+// }
+// else if(s==2){
+//     console.log("HA IKKI BOR");
+// }
+// else{
+//     console.log("YOK IKKI");
+// }
+
+//while-10
+
+// let son = +prompt("Son kiritin")
+
+
+// i=1
+
+
+// while(i<=son){
+//     i++
+//    i++
+//    console.log(`${son-i}`)
+    
+// }
+
+// while-11 tushunmadim
+
+// while-12
+// let son = +prompt("Son kiritin")
+
+// i=1
+
+// let s = 0
+
+// while(i<=son){
+    
+//    if(son%i==0){
+//     s++
+//    }
+//    i++
+// }
+// if(s==2){
+//     console.log("Son tub");
+// }
+// else{
+//     console.log("Son tub emas");
+// }
